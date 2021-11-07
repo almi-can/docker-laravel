3b0bb656a899d4b4a00ec99a64358579450e2797
almi-can <rmrtjut13@gmail.com>
2021-11-07 22:02:30 +0900

    edit network document

--- backend/resources/views/network.blade.php
+++ backend/resources/views/network.blade.php
@@ -4,11 +4,13 @@
   <body>
     <h1>ネットワーク</h1>
     <h2>Laravel Tips</h2>
-    backendフォルダ内の権限を777にしないとlogのエラーが起きる
-    7.0と8.0でrouteの書き方が違うので注意
-    viewはextendで継承、includeで埋め込み    
+    backendフォルダ内の権限を777にしないとlogのエラーが起きる</br>
+    git clone してもvendorフォルダ（laravel本体）はコピーされないのでappコンテナでcomposer installする</br>
+    7.0と8.0でrouteの書き方が違うので注意</br>
+    viewはextendで継承、includeで埋め込み
     <h2>設定用ファイル</h2>
     <a href="files/containers.tar.gz" download>お家docker用のファイル</a><br>
+    ※パス工事中
     <h2>ネットワーク構成図</h2>
     <img src="images/home-network.png"></br>
     この構成図はpythonの
